9a6a5434fa390b40a9ac846ce68a9ccdd22cae91
Arefeh Rastegar <arefeh.rastegar@snapp.cab>
2025-04-13 13:30:27 +0330

    add 5th day exercises

---
+++ day5-files.py
@@ -0,0 +1,13 @@
+#practice 1
+with open("notes.txt","w") as file:
+    file.write("Python is fun.\nLearning step by step.\nFiles are easy!")
+
+#practice 2
+with open("notes.txt","r") as file:
+    for line in file.readlines():
+        print(line)
+
+#practice 3
+name = input("please tell me your name:")
+with open("users.txt","a") as file:
+    file.write(f"{name}\n")
