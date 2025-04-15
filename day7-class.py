fbc638da9ab2b9c092f609ed87d68a8691818cf8
Arefeh Rastegar <arefeh.rastegar@snapp.cab>
2025-04-15 12:19:15 +0330

    edit

--- day7-class.py
+++ day7-class.py
@@ -8,13 +8,13 @@ class Person:
         print(f"The client name is {self.name.title()} and the age is {self.age}")
 
 class Employee(Person):
-    def __init__(self,job_title):
+    def __init__(self,job_title,name,age):
+        super().__init__(name,age)
         self.job_title = job_title
 
     def job(self):
+        super().introduce()
         print(f"{self.name.title()}'s job is {self.job_title}")
 
-p = Person("azadeh", 30)
-p.introduce()
-e = Employee("engineer")
+e = Employee("engineer","azadeh", 30)
 e.job()
