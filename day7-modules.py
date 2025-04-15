f925cb2739adb5aa4131558e9aad469d709b9674
Arefeh Rastegar <arefeh.rastegar@snapp.cab>
2025-04-15 15:28:59 +0330

    edit

--- day7-modules.py
+++ day7-modules.py
@@ -3,6 +3,7 @@ import random
 import datetime as dt
 import os
 import requests
+import json
 
 print(math.sqrt(25))
 print(random.randint(1,10))
@@ -10,3 +11,5 @@ print(dt.datetime.now())
 print(os.getcwd())
 response = requests.get("https://api.github.com")
 print(response.status_code)
+res = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+print(res.status_code)
