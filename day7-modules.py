45e7c1360db2798a7d6503316556e13e76776187
Arefeh Rastegar <arefeh.rastegar@snapp.cab>
2025-04-15 15:16:03 +0330

    add requests

--- day7-modules.py
+++ day7-modules.py
@@ -2,8 +2,11 @@ import math
 import random
 import datetime as dt
 import os
+import requests
 
 print(math.sqrt(25))
 print(random.randint(1,10))
 print(dt.datetime.now())
 print(os.getcwd())
+response = requests.get("https://api.github.com")
+print(response.status_code)
