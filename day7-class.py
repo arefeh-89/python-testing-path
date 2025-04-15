e9283568cf1e2b42c88eed4f9895bde18525b1d0
Arefeh Rastegar <arefeh.rastegar@snapp.cab>
2025-04-15 13:44:06 +0330

    add some more exercises

--- day7-class.py
+++ day7-class.py
@@ -18,3 +18,42 @@ class Employee(Person):
 
 e = Employee("engineer","azadeh", 30)
 e.job()
+
+#practice 2
+class Vehicle:
+    def __init__(self,brand,model):
+        self.brand = brand
+        self.model = model
+    
+    def info(self):
+        print(f"The brand is {self.brand.title()} and it's model is {self.model}.")
+
+class Car(Vehicle):
+    def __init__(self,brand,model,num_doors):
+        super().__init__(brand,model)
+        self.num_doors = num_doors
+
+    def info(self):
+        super().info()
+        print(f"This car has {self.num_doors} doors.")
+
+c = Car("benz","2020",4).info()
+
+#practice 3
+
+class Employee:
+    def __init__(self,name,salary):
+        self.name = name
+        self.salary = salary
+
+class Manager(Employee):
+    def __init__(self,name,salary,team_size):
+        super().__init__(name,salary)
+        self.team_size = team_size
+
+    def show_details(self):
+        print(f"The employee name is {self.name.title()} and salary is {self.salary}.")
+        print(f"her team is included {self.team_size} members!")
+
+m = Manager("baran","3000 $",5)
+m.show_details()
